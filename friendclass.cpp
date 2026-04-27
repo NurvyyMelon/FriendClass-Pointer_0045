2545cef15486d07338b9ab4b92ca9f7c444d1eef
NurvyyMelon <adinnaufal9@gmail.com>
2026-04-27 10:59:30 +0700

    first commit

---
+++ friendclass.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa; // Forward declaration
+
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa; // Memberikan akses ke class siswa
+};
