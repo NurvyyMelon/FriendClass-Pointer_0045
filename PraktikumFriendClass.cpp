b20d3abcb66e68b7e5b68d9b29442bb4180e6240
NurvyyMelon <adinnaufal9@gmail.com>
2026-04-27 11:31:31 +0700

    Menambahkan fungsi input untuk kelas LayangLayang guna menerima input diagonal dan sisi

--- PraktikumFriendClass.cpp
+++ PraktikumFriendClass.cpp
@@ -8,3 +8,16 @@ class LayangLayang {
 private:
     float d1, d2; // diagonal
     float s1, s2; // sisi
+
+    public:
+    void input() {
+        cout << "=== Layang-Layang ===" << endl;
+        cout << "Masukkan diagonal 1: ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2: ";
+        cin >> d2;
+        cout << "Masukkan sisi 1: ";
+        cin >> s1;
+        cout << "Masukkan sisi 2: ";
+        cin >> s2;
+    }
